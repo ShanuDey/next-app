fd20e26611f77cf67aff73fdf985a230f17c906a
Shanu Dey <sd4shanudey@gmail.com>
2022-01-22 13:30:34 +0530

    fetch 10 pokemons by random offset
    Signed-off-by: Shanu Dey <sd4shanudey@gmail.com>

--- components/PokeList.js
+++ components/PokeList.js
@@ -1,12 +1,34 @@
 import { PokeCard } from '../components/PokeCard';
 import styles from '../styles/PokeList.module.css';
 
-import React from 'react';
+import React, { useState } from 'react';
+import { useEffect } from 'react/cjs/react.development';
+import axios from 'axios';
 
 export const PokeList = () => {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://pokeapi.co/api/v2/pokemon?limit=10&offset=${Math.floor(
+          Math.random() * 500 + 1
+        )}`
+      )
+      .then(function (response) {
+        console.log(response);
+        setPokemons(response.data.results);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className={styles.grid}>
-      <PokeCard />
+      {pokemons.map((pokemon) => (
+        <PokeCard pokemon={pokemon} key={pokemon.name} />
+      ))}
     </div>
   );
 };
