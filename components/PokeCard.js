df2d08aacd3cec47d8515905688dfe1f4a8d52e0
Shanu Dey <sd4shanudey@gmail.com>
2022-01-22 13:30:46 +0530

    implementing pokecards
    Signed-off-by: Shanu Dey <sd4shanudey@gmail.com>

--- components/PokeCard.js
+++ components/PokeCard.js
@@ -1,12 +1,13 @@
 import Link from 'next/link';
 import styles from '../styles/Card.module.css';
 
-export const PokeCard = () => {
+export const PokeCard = ({ pokemon }) => {
+  const id = pokemon.url.split('pokemon/')[1];
   return (
-    <Link href='/'>
+    <Link href={`/pokemons/${id}`}>
       <a className={styles.card}>
-        <h2>Pikachu &rarr;</h2>
-        <p>Learn more about Pikachu</p>
+        <h2>{pokemon.name} &rarr;</h2>
+        <p>Learn more about {pokemon.name}</p>
       </a>
     </Link>
   );
